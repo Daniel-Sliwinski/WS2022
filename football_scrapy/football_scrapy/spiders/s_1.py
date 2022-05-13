da8c3672c1fcc8ccb24d641613e0b0ce25448537
Daniel-Sliwinski <dsliwek@gmail.com>
2022-05-13 14:20:18 +0200

    Complete soup, now working on scrapy (.com)

--- football_scrapy/football_scrapy/spiders/s_1.py
+++ football_scrapy/football_scrapy/spiders/s_1.py
@@ -5,12 +5,12 @@ class Link(scrapy.Item):
 
 class FootballSpider(scrapy.Spider):
     name = 'links'
-    allowed_domains = ['https://www.transfermarkt.pl/']
+    allowed_domains = ['https://www.transfermarkt.com/']
     #range of years to scrape
     years = range(2015,2022,1)
 
     #list of links with years
-    start_urls = ['https://www.transfermarkt.pl/premier-league/startseite/wettbewerb/GB1/plus/?saison_id={}'.format(object) for object in years]
+    start_urls = ['https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1/plus/?saison_id={}'.format(object) for object in years]
 
     def parse(self, response):
 
@@ -20,5 +20,5 @@ class FootballSpider(scrapy.Spider):
 
         for s in selection:
             l = Link()
-            l['link'] = 'https://www.transfermarkt.pl' + s.get()
+            l['link'] = 'https://www.transfermarkt.com' + s.get()
             yield l
